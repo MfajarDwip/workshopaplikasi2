6e17b3d2fd4661db7b7d42fa1f9715dfc9fb0098
Muhammad Fajar Dwi Putra <niceteamwork993@gmail.com>
2024-02-13 14:49:30 +0700

    commit pertama

---
+++ bin/dart1.dart
@@ -0,0 +1,15 @@
+// import 'package:dart1/dart1.dart' as dart1;
+
+// void main(List<String> arguments) {
+//   print('Hello world: ${dart1.calculate()}!');
+// }
+
+import 'dart:io';
+
+void main() {
+ int p = 19;
+ String a = "$p";
+ print("haii"+ a);
+}
+
+
